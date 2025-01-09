1bfbbc34fbd42c1d9ded006cb93eb9ed43edf782
leenalmajz <leenmajzoub20@gmail.com>
2025-01-09 11:36:02 +0100

    Day 1 completed

--- day-1/exercise_2.py
+++ day-1/exercise_2.py
@@ -2,6 +2,8 @@ from typing import List, Tuple
 from exercise_1 import read_input
 
 def total_similarity_score(list1: List[int], list2: List[int]) :
+    ''' Calculate the sum of the product of the number of times each number in list1 appears in list2 and the number
+        for example: if list1 = [1,2,3] and list2 = [1,1,2,2,2,3], the similarity score will be 1*2 + 2*3 + 3*1 = 11'''
 
     similarity_score = 0
     for number in list1:
