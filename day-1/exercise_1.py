d1f7ef8bb4bc381fea399da00f4cdac42a4a1158
leenalmajz <leenmajzoub20@gmail.com>
2025-01-09 10:53:38 +0100

    day 1 exercise 1 complete

---
+++ day-1/exercise_1.py
@@ -0,0 +1,37 @@
+from typing import List, Tuple
+
+def sum_of_distances(list1: List[int], list2: List[int]) -> int:
+    '''Find the total distance between two lists of integers'''
+
+    list1.sort()
+    list2.sort()
+
+    difference = [abs(a-b) for a,b in zip(list1, list2)]
+    
+    total_distance = sum(difference)
+
+    return total_distance
+
+def read_input (file_path: str) -> Tuple[List[int], List[int]]:
+    '''Read the input file and return a tuple of two sorted lists of integers'''
+
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+    
+    list1 = []
+    list2 = []
+
+    for location_IDs in data:
+        location_ID = location_IDs.strip().split()
+        list1.append(int(location_ID[0]))
+        list2.append(int(location_ID[1]))
+    
+    list1.sort(), list2.sort()
+    
+    return list1, list2
+\
+list1, list2 = read_input('day-1/input.txt')
+
+total_distance = sum_of_distances(list1, list2)
+
+print(total_distance)
